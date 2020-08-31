44a68ede1fa28dac85db0f64b481a8bfa03c7d94
André <ruland@outlook.de>
2020-08-31 11:44:14 +0200

    Singelton Threadsafe

---
+++ HalloSingelton/HalloSingelton/Logger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HalloSingelton
+{
+    class Logger
+    {
+        private static Logger _instance = null;
+        private static Object _syncObj = new Object();
+
+        public static Logger Instance
+        {
+            get
+            {
+                lock (_syncObj)
+                {
+                    if (_instance == null)
+                        _instance = new Logger();
+                }
+                return _instance;
+            }
+        }
+
+        private Logger()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("CTOR");
+            Console.ResetColor();
+        }
+
+        public void Log(string txt)
+        {
+            Console.WriteLine($"[{DateTime.Now}] {txt}");
+        }
+    }
+}
---
+++ HalloSingelton/HalloSingelton/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloSingelton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Singelton");
+
+            //var logger = new Logger();
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Task.Run(() => Logger.Instance.Log($"Test #{i}"));
+            }
+
+            //Logger.Instance.Log("Hallo Logger");
+            //Logger.Instance.Log("Hallo Logger");
+            //Logger.Instance.Log("Hallo Logger");
+
+            Console.WriteLine("Ende");
+            Console.ReadKey();
+        }
+    }
+}
