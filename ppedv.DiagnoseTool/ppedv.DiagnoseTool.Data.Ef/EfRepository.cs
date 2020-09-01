3471bf39b4e24d2085e2bf197dedc1d7d2f866f7
André <ruland@outlook.de>
2020-09-01 15:20:14 +0200

    Business Layer prepared und Repository erstellt

---
+++ ppedv.DiagnoseTool/ppedv.DiagnoseTool.Data.Ef/EfRepository.cs
@@ -0,0 +1,53 @@
+﻿using ppedv.DiagnoseTool.Model;
+using ppedv.DiagnoseTool.Model.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.DiagnoseTool.Data.Ef
+{
+    public class EfRepository : IRepository
+    {
+        private EfContext context = new EfContext();
+
+        public void Add<T>(T entity) where T : Entity
+        {
+            //if (typeof(T) == typeof(Arzt))
+            //    context.Ärzte.Add(entity as Arzt);
+            context.Set<T>().Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : Entity
+        {
+            context.Set<T>().Remove(entity);
+        }
+
+        public IEnumerable<T> GetAll<T>() where T : Entity
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public T GetById<T>(int id) where T : Entity
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public int SaveAll()
+        {
+            return context.SaveChanges();
+        }
+
+        public void Update<T>(T entity) where T : Entity
+        {
+            var loaded = GetById<T>(entity.Id);
+            if (loaded == null)
+                throw new ObjectNotFoundException();
+
+
+            context.Entry(loaded).CurrentValues.SetValues(entity);
+        }
+    }
+}
---
+++ ppedv.DiagnoseTool/ppedv.DiagnoseTool.Logik/Core.cs
@@ -0,0 +1,18 @@
+﻿using ppedv.DiagnoseTool.Data.Ef;
+using ppedv.DiagnoseTool.Model;
+using System;
+using System.Collections.Generic;
+
+namespace ppedv.DiagnoseTool.Logik
+{
+    public class Core
+    {
+
+
+        public IEnumerable<Patient> GetPatientenByFacharztrichtung(string facharztrichtung)
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}
