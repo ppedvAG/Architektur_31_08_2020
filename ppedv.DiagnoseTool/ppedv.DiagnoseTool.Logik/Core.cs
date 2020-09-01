fd54bc390bca77029b0ad2b3478245001522ec2c
André <ruland@outlook.de>
2020-09-01 16:16:48 +0200

    Consolen UI fertig, all done

--- ppedv.DiagnoseTool/ppedv.DiagnoseTool.Logik/Core.cs
+++ ppedv.DiagnoseTool/ppedv.DiagnoseTool.Logik/Core.cs
@@ -1,18 +1,29 @@
 ﻿using ppedv.DiagnoseTool.Data.Ef;
 using ppedv.DiagnoseTool.Model;
+using ppedv.DiagnoseTool.Model.Contracts;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ppedv.DiagnoseTool.Logik
 {
     public class Core
     {
+        public IRepository Repository { get; private set; }
+
+        public Core(IRepository repository)
+        {
+            Repository = repository;
+        }
 
 
         public IEnumerable<Patient> GetPatientenByFacharztrichtung(string facharztrichtung)
         {
-
-            throw new NotImplementedException();
+            return Repository.GetAll<Arzt>().Where(x => x.FacharztRichtung
+                                            .Contains(facharztrichtung))
+                                            .SelectMany(x => x.Diagnosen)
+                                            .Select(x => x.Patient);
         }
     }
 }
---
+++ ppedv.DiagnoseTool/ppedv.DiagnoseTool.UI.DevConsole/Program.cs
@@ -0,0 +1,46 @@
+﻿using ppedv.DiagnoseTool.Logik;
+using ppedv.DiagnoseTool.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.DiagnoseTool.UI.DevConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** DiagnoseTool v0.1 ***");
+
+            var core = new Core(new Data.Ef.EfRepository());
+
+            foreach (var p in core.Repository.GetAll<Patient>())
+            {
+                ShowPatient(p);
+            }
+            
+            Console.WriteLine("********************************************************");
+
+            foreach (var p in core.GetPatientenByFacharztrichtung("FacharztRichtungc6c58d74-2a9c-4b5d-9e9b-2333f4a82e90"))
+            {
+                ShowPatient(p);
+            }
+
+
+
+            Console.WriteLine("Ende");
+            Console.ReadLine();
+        }
+
+        private static void ShowPatient(Patient p)
+        {
+            Console.WriteLine($"Name: {p.Name} [{p.GebDatum:d}] {p.Geschlecht}");
+            foreach (var d in p.Diagnosen)
+            {
+                Console.WriteLine($"\t{d.Code} - {d.Beschreibung} Arzt: {d.Arzt.Name} [{d.Arzt.FacharztRichtung}]");
+            }
+        }
+    }
+}
