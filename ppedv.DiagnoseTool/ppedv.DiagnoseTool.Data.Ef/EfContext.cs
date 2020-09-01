627dc773d14195916f39acaefa342f2478945c08
André <ruland@outlook.de>
2020-09-01 11:28:44 +0200

    EntityFrameworls als ein Datanlayer hinzugefügt

---
+++ ppedv.DiagnoseTool/ppedv.DiagnoseTool.Data.Ef/EfContext.cs
@@ -0,0 +1,13 @@
+﻿using ppedv.DiagnoseTool.Model;
+using System.Data.Entity;
+
+namespace ppedv.DiagnoseTool.Data.Ef
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<Arzt> Ärzte { get; set; }
+        public DbSet<Patient> Patienten { get; set; }
+        public DbSet<Diagnose> Diagnosen { get; set; }
+
+    }
+}
